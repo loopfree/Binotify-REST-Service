35d73002b5090ac11b08937b3b03d7ff778f0f42
Steven Wen <73114262+loopfree@users.noreply.github.com>
2022-11-25 22:13:05 +0700

    feat: including subscription list endpoint into a file

--- bin/endpoints/endpoint.js
+++ bin/endpoints/endpoint.js
@@ -1,1 +1,6 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const subscriptionlist_1 = require("./subscriptionlist");
+exports.default = {
+    getSubscriptionList: subscriptionlist_1.getSubscriptionList
+};
---
+++ src/endpoints/endpoint.ts
@@ -0,0 +1,5 @@
+import { getSubscriptionList } from "./subscriptionlist";
+
+export default{
+    getSubscriptionList
+}
