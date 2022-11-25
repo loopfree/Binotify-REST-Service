49849fe395405440d4d6b47d055a2c2934ff8af7
Steven Wen <73114262+loopfree@users.noreply.github.com>
2022-11-25 22:13:57 +0700

    feat: start server
    - starting the express server
    - including the subscription list endpoint function

--- bin/index.js
+++ bin/index.js
@@ -4,9 +4,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const endpoint_1 = __importDefault(require("./endpoints/endpoint"));
 const app = (0, express_1.default)();
 const port = 3000;
 app.get("/subscriptionlist", (req, res) => {
+    endpoint_1.default.getSubscriptionList(req, res);
 });
 app.listen(port, () => {
     console.log(`App is running on :${port}`);
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import express, { Application } from "express";
+import { Client } from "soap";
+
+import endpoint from "./endpoints/endpoint";
 
 const app: Application = express();
 
 const port: number = 3000;
 
 app.get("/subscriptionlist",(req, res) => {
-
+    endpoint.getSubscriptionList(req, res);
 });
 
 app.listen(port, () => {
