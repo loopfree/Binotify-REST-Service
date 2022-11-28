1199711420cafdd3ca1836e15e6b403746baf35f
Steven Wen <73114262+loopfree@users.noreply.github.com>
2022-11-28 21:29:16 +0700

    feat: allow admin to approve subscription

---
+++ bin/endpoints/approvesubscription.js
@@ -0,0 +1,24 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.approveSubscription = void 0;
+const soapwrapper_1 = require("./../helper/soapwrapper");
+function approveSubscription(req, res) {
+    return __awaiter(this, void 0, void 0, function* () {
+        console.log("Argument: ", req.body);
+        const url = "http://localhost:8042/admin?wsdl";
+        const client = yield (0, soapwrapper_1.createSoapClient)(url);
+        const args = req.body;
+        // Memanggil fungsi SOAP bernama approveSubscriptionRequest
+        res.json(yield (0, soapwrapper_1.callSoapMethod)(client, "approveSubscriptionRequest", args));
+    });
+}
+exports.approveSubscription = approveSubscription;
---
+++ src/endpoints/approvesubscription.ts
@@ -0,0 +1,19 @@
+import { Response, Request } from "express";
+import { Client } from "soap";
+import { createSoapClient, callSoapMethod } from "./../helper/soapwrapper";
+
+async function approveSubscription(req: Request, res: Response) {
+    console.log("Argument: ", req.body);
+    const url = "http://localhost:8042/admin?wsdl";
+
+    const client: Client = await createSoapClient(url) as Client;
+
+    const args = req.body;
+    
+    // Memanggil fungsi SOAP bernama approveSubscriptionRequest
+    res.json(await callSoapMethod(client, "approveSubscriptionRequest", args));
+}
+
+export {
+    approveSubscription
+}
