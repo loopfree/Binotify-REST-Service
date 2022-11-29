56219d4019d48d1e22fd97e9d697a92212654a17
Steven Wen <73114262+loopfree@users.noreply.github.com>
2022-11-30 01:19:23 +0700

    feat: add login and register to endpoint collection

--- bin/endpoints/endpoint.js
+++ bin/endpoints/endpoint.js
@@ -3,8 +3,16 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const subscriptionlist_1 = require("./subscriptionlist");
 const approvesubscription_1 = require("./approvesubscription");
 const declineSubscription_1 = require("./declineSubscription");
+const premiumSingers_1 = require("./premiumSingers");
+const premiumSongs_1 = require("./premiumSongs");
+const login_1 = require("./login");
+const register_1 = require("./register");
 exports.default = {
     getSubscriptionList: subscriptionlist_1.getSubscriptionList,
     approveSubscription: approvesubscription_1.approveSubscription,
-    declineSubscription: declineSubscription_1.declineSubscription
+    declineSubscription: declineSubscription_1.declineSubscription,
+    getPremiumSingers: premiumSingers_1.getPremiumSingers,
+    getPremiumSongs: premiumSongs_1.getPremiumSongs,
+    login: login_1.login,
+    register: register_1.register
 };
--- src/endpoints/endpoint.ts
+++ src/endpoints/endpoint.ts
@@ -3,11 +3,15 @@ import { approveSubscription } from "./approvesubscription";
 import { declineSubscription } from "./declineSubscription";
 import { getPremiumSingers } from "./premiumSingers";
 import { getPremiumSongs } from "./premiumSongs";
+import { login } from "./login";
+import { register } from "./register";
 
 export default{
     getSubscriptionList,
     approveSubscription,
     declineSubscription,
     getPremiumSingers,
-    getPremiumSongs
+    getPremiumSongs,
+    login,
+    register
 }
