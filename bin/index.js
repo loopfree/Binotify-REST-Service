d16dbbaaee8588d272de42b76354e9ec103c0073
Steven Wen <73114262+loopfree@users.noreply.github.com>
2022-11-30 01:20:58 +0700

    feat: add login, register, and jwt
    - add login endpoint
    - add register endpoint
    - implement jwt

--- bin/index.js
+++ bin/index.js
@@ -5,19 +5,33 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
+const jwt_express_1 = __importDefault(require("jwt-express"));
 const endpoint_1 = __importDefault(require("./endpoints/endpoint"));
 const port = 3000;
 const app = (0, express_1.default)();
 app.use((0, cors_1.default)());
 app.use(express_1.default.json());
-app.get("/subscription/list", (req, res) => {
+app.use(jwt_express_1.default.init("steven wen pro :v"));
+app.post("authenticate/login", jwt_express_1.default.active(), (req, res) => {
+    endpoint_1.default.login(req, res);
+});
+app.post("authenticate/register", jwt_express_1.default.active(), (req, res) => {
+    endpoint_1.default.register(req, res);
+});
+app.get("/subscription/list", jwt_express_1.default.active(), (req, res) => {
     endpoint_1.default.getSubscriptionList(req, res);
 });
-app.post("/subscription/approve", (req, res) => {
+app.post("/subscription/approve", jwt_express_1.default.active(), (req, res) => {
     endpoint_1.default.approveSubscription(req, res);
 });
-app.post("/subscription/decline", (req, res) => {
+app.post("/subscription/decline", jwt_express_1.default.active(), (req, res) => {
     endpoint_1.default.declineSubscription(req, res);
+});
+app.post("/premium_singer/list", jwt_express_1.default.active(), (req, res) => {
+    endpoint_1.default.getPremiumSingers(req, res);
+});
+app.post("/premium_singer/song", jwt_express_1.default.active(), (req, res) => {
+    endpoint_1.default.getPremiumSingers(req, res);
 });
 app.listen(port, () => {
     console.log(`App is running on :${port}`);
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from 'cors';
+import jwt from "jwt-express";
 
 import endpoint from "./endpoints/endpoint";
 
@@ -8,24 +9,33 @@ const port = 3000;
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(jwt.init("steven wen pro :v"));
 
-app.get("/subscription/list",(req, res) => {
+app.post("authenticate/login", jwt.active(), (req, res) => {
+    endpoint.login(req, res);
+});
+
+app.post("authenticate/register", jwt.active(), (req, res) => {
+    endpoint.register(req, res);
+})
+
+app.get("/subscription/list", jwt.active(), (req, res) => {
     endpoint.getSubscriptionList(req, res);
 });
 
-app.post("/subscription/approve", (req, res) => {
+app.post("/subscription/approve", jwt.active(), (req, res) => {
     endpoint.approveSubscription(req, res);
 })
 
-app.post("/subscription/decline", (req, res) => {
+app.post("/subscription/decline", jwt.active(), (req, res) => {
     endpoint.declineSubscription(req, res);
 })
 
-app.post("/premium_singer/list", (req, res) => {
+app.post("/premium_singer/list", jwt.active(), (req, res) => {
     endpoint.getPremiumSingers(req, res);
 })
 
-app.post("/premium_singer/song", (req, res) => {
+app.post("/premium_singer/song", jwt.active(), (req, res) => {
     endpoint.getPremiumSingers(req, res);
 })
 
