45d5fa23684047f9cd2c25bef55db08db129e8ad
Steven Wen <73114262+loopfree@users.noreply.github.com>
2022-12-02 09:30:05 +0700

    feat: integrated api key into decline

--- src/endpoints/declineSubscription.ts
+++ src/endpoints/declineSubscription.ts
@@ -1,5 +1,7 @@
 import { Response, Request } from "express";
 import { Client } from "soap";
+
+import { apikey } from "./../helper/apikey";
 import { createSoapClient, callSoapMethod } from "./../helper/soapwrapper";
 
 async function declineSubscription(req: Request, res: Response) {
@@ -8,6 +10,7 @@ async function declineSubscription(req: Request, res: Response) {
     const client: Client = await createSoapClient(url) as Client;
 
     const args = req.body;
+    args["apiKey"] = apikey;
     
     // Memanggil fungsi SOAP bernama approveSubscriptionRequest
     res.json(await callSoapMethod(client, "declineSubscriptionRequest", args));
